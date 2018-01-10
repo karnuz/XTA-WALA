ac766d03d0ec6b2e80011c80ead13bef5c665f58
Himanshu <himanshu.ecko@gmail.com>
2018-01-10 19:10:25 +0530

    TestSuite for Extractor

---
+++ tests/TestSuite/TestSuite.java
@@ -0,0 +1,13 @@
+package tests.TestSuite;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+
+@Suite.SuiteClasses({
+   testDataFlowfacts.class
+})
+
+public class TestSuite {   
+}
